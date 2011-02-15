4797d6f875110cf3b45d17e188589ab297e9df34
helloanant007@gmail.com <helloanant007@gmail.com@90a6c7a1-1889-9cb4-3ffc-9decf77399ad>
2011-02-15 23:37:53 +0000

    The HTTP GET testing in the bolierplate code

--- trunk/depot/client/android/src/edu/stanford/cs247/StuddyBuddy.java
+++ trunk/depot/client/android/src/edu/stanford/cs247/StuddyBuddy.java
@@ -2,6 +2,22 @@ package edu.stanford.cs247;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * 
+ * @author Anant Bhardwaj
+ * @date: 2/15/2011
+ * 
+ * Basic boilerplate code.
+ *
+ */
 
 public class StuddyBuddy extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +25,26 @@ public class StuddyBuddy extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        String content="";
+        try{
+            URL url = new URL ("http://stanford.edu/~anantb/cgi-bin/test.php?name=Anant");           
+            //url.openConnection();
+            
+            InputStream in = url.openStream();            
+            InputStreamReader inReader = new InputStreamReader(in);            
+            BufferedReader br = new BufferedReader(inReader);
+            String j;
+            while((j = br.readLine())!=null)  {
+                content=content+j;
+
+            }
+        }catch(MalformedURLException mle) {
+            content = mle.getMessage();
+        }catch(Exception e){
+            content = e.getMessage();
+        }
+        TextView tv = new TextView(this);
+        tv.setText(content);
+        setContentView(tv);
     }
 }
