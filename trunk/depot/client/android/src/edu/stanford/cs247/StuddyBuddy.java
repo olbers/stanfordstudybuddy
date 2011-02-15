3d0e2b3a64a92f47e70562d47375e51aad8884c7
helloanant007@gmail.com <helloanant007@gmail.com@90a6c7a1-1889-9cb4-3ffc-9decf77399ad>
2011-02-15 22:47:10 +0000

    The client project placeholder

---
+++ trunk/depot/client/android/src/edu/stanford/cs247/StuddyBuddy.java
@@ -0,0 +1,13 @@
+package edu.stanford.cs247;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StuddyBuddy extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
