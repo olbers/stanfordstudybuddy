c199eb5ee6604176e02dc1955380555fa8bb2ccb
helloanant007@gmail.com <helloanant007@gmail.com@90a6c7a1-1889-9cb4-3ffc-9decf77399ad>
2011-02-17 17:20:15 +0000

    added text below the icon

--- trunk/depot/client/web-prototype/index.php
+++ trunk/depot/client/web-prototype/index.php
@@ -13,23 +13,73 @@ Study Buddy Main Page
 
 
 <div align="center">
-<table style="border-spacing:15px;">
+<table class="icon_layout">
 <tr>
 <td>
-<div style="background-image: url(resources/images/icon_myprofile.png); background-repeat:no-repeat; width:100px; height:100px; border:solid thin #CCC; cursor:pointer;" />
+<table class="icon">
+<tr>
+<td class="icon">
+<div style="background-image: url(resources/images/icon_myprofile.png); background-repeat:no-repeat; 
+width:100px; height:100px; cursor:pointer;" 
+onclick="window.location = &quot;checkin.php&quot;;"/>
+</td>
+</tr>
+<tr>
+<td class="icon">
+<span class="icon_text">My Profile</span>
+</td>
+</tr>
+</table>
 </td>
 <td>
+<table class="icon">
+<tr>
+<td class="icon">
 <div style="background-image: url(resources/images/icon_checkinout.png); background-repeat:no-repeat; 
-width:100px; height:100px; border:solid thin #CCC; cursor:pointer;" 
-onclick="window.location = &quot;checkin.php&quot;;" />
+width:100px; height:100px; cursor:pointer;" 
+onclick="window.location = &quot;checkin.php&quot;;"/>
+</td>
+</tr>
+<tr>
+<td class="icon">
+<span class="icon_text">Check-In</span>
+</td>
+</tr>
+</table>
 </td>
 </tr>
 <tr>
 <td>
-<div style="background-image: url(resources/images/icon_search.png); background-repeat:no-repeat; width:100px; height:100px; border:solid thin #CCC; cursor:pointer;" />
+<table class="icon">
+<tr>
+<td class="icon">
+<div style="background-image: url(resources/images/icon_search.png); background-repeat:no-repeat; 
+width:100px; height:100px; cursor:pointer;" 
+onclick="window.location = &quot;checkin.php&quot;;"/>
+</td>
+</tr>
+<tr>
+<td class="icon">
+<span class="icon_text">Search</span>
+</td>
+</tr>
+</table>
 </td>
 <td>
-<div style="background-image: url(resources/images/icon_campusdata.png); background-repeat:no-repeat; width:100px; height:100px; border:solid thin #CCC; cursor:pointer;" />
+<table class="icon">
+<tr>
+<td class="icon">
+<div style="background-image: url(resources/images/icon_campusdata.png); background-repeat:no-repeat; 
+width:100px; height:100px; cursor:pointer;" 
+onclick="window.location = &quot;checkin.php&quot;;"/>
+</td>
+</tr>
+<tr>
+<td class="icon">
+<span class="icon_text">Campus Data</span>
+</td>
+</tr>
+</table>
 </td>
 </tr>
 </table>  
