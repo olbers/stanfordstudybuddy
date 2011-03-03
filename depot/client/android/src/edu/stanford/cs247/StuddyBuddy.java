a0e987bd4d9b1c1212a5269fe75b1d1136cff7b8
helloanant007 <helloanant007@gmail.com>
2011-03-03 00:24:58 +0000

    small change

--- depot/client/android/src/edu/stanford/cs247/StuddyBuddy.java
+++ depot/client/android/src/edu/stanford/cs247/StuddyBuddy.java
@@ -27,7 +27,7 @@ public class StuddyBuddy extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
+        setContentView(R.layout.main);
         //String content="";
         
        
