e1d4f468920fbdeb6683af6f3bca32fdee4592a8
helloanant007@gmail.com <helloanant007@gmail.com@90a6c7a1-1889-9cb4-3ffc-9decf77399ad>
2011-03-14 18:24:59 +0000

    adding blurb

--- trunk/website/people.php
+++ trunk/website/people.php
@@ -3,6 +3,7 @@ require_once("resources/inc/header.php");
 ?>
     
 <td class="main-panel" style="text-align:left">
+<div class="box">
 <h1>Study Buddy Team</h1>
 <hr />
 <table>
@@ -12,9 +13,9 @@ require_once("resources/inc/header.php");
 </td>
 <td style="padding:20px;">
     <div class="box">
-    Anant Bhardwaj <br />
+    <span style="font-weight:bold">Anant Bhardwaj</span> <br />
     Graduate Student <br />
-    Human Computer Interaction, Department od Computer Science <br />
+    HCI, Department of Computer Science <br />
     Stanford University <br />
     </div> 
 </td>
@@ -25,7 +26,7 @@ require_once("resources/inc/header.php");
 </td>
 <td style="padding:20px;">
     <div class="box">
-    Dave Luciano <br />
+    <span style="font-weight:bold">Dave Luciano</span> <br />
     B.S. Candidate, Computer Science <br />
     Minor Candidate in Geological and Environmental Sciences <br />
     Stanford University | Class of 2012
@@ -38,27 +39,28 @@ require_once("resources/inc/header.php");
 </td>
 <td style="padding:20px;">
     <div class="box">
-    Jaiwon Rhi <br />
-    B.S. Candidate, Computer Science <br />
-    Minor Candidate in Geological and Environmental Sciences <br />
-    Stanford University | Class of 2012
+    <span style="font-weight:bold">Jaiwon Rhi</span> <br />
+    Coterminal Graduate Student <br />
+    Computer Science, Communication <br />
+    Class of 2011
     </div>  
 </td>
 </tr>
 <tr>
 <td>
-<img src="resources/images/people/anant.jpg" alt="anant.jpg" class="profile-pic" />
+<img src="resources/images/people/mike.jpg" alt="mike.jpg" class="profile-pic" />
 </td>
 <td style="padding:20px;">
 <div class="box">
-    Mike Ortiz <br />
-    B.S. Candidate, Computer Science <br />
-    Minor Candidate in Geological and Environmental Sciences <br />
-    Stanford University | Class of 2012
+    <span style="font-weight:bold">Mike Ortiz</span> <br />
+    B.S. Candidate, Computer Science  <br />
+	Stanford University | Class of 2011 <br/>
     </div>  
    
 </td>
 </tr>
+</table>
+</div>
 </td>
 <?php
 require_once("resources/inc/footer.php");
